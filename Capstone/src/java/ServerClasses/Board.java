db1133bec1e2968e2b4f800d641cd1fbace5d704
ltspeedy <ltspeedy@IT065098.massey.ac.nz>
2014-07-23 15:26:24 +1200

    Added previous files

---
+++ Capstone/src/java/ServerClasses/Board.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ServerClasses;
+
+/**
+ *
+ * @author Taylor
+ */
+public class Board {
+    public void getBoard(){
+        return;
+    }
+    public void setBoard(){
+        return;
+    }
+    public void changeController(String nodename, String newController){
+        return;
+        
+    }
+    public void changeTroops(String nodename, int change){
+        return;
+        
+    }  
+}

a13dd0c7e6083653bf6cae6bf480a16b0ff80ac8
Taylor Hamling <0603671@live.com>
2014-07-23 18:29:39 +1200

    Board Update
    Updating with Patricks stuff

---
+++ Capstone/src/java/ServerClasses/Node.java
@@ -0,0 +1,57 @@
+package server;
+
+public class Node {
+	private String territoy;
+	private String controllingPlayer;
+	private String continent;
+	private int troops;
+	private String[] adjacentNodes;
+
+	public Node(String territory, String player, String continent, int troops, String[] adjacentNodes){
+		this.setTerritoy(territory);
+		this.setControllingPlayer(player);
+		this.setContinent(continent);
+		this.setTroops(troops);
+		this.setAdjacentNodes(adjacentNodes);
+	}
+
+	public String getControllingPlayer() {
+		return controllingPlayer;
+	}
+
+	public void setControllingPlayer(String controllingPlayer) {
+		this.controllingPlayer = controllingPlayer;
+	}
+
+	public String getTerritoy() {
+		return territoy;
+	}
+
+	public void setTerritoy(String territoy) {
+		this.territoy = territoy;
+	}
+
+	public int getTroops() {
+		return troops;
+	}
+
+	public void setTroops(int troops) {
+		this.troops = troops;
+	}
+
+	public String[] getAdjacentNodes() {
+		return adjacentNodes;
+	}
+
+	public void setAdjacentNodes(String[] adjacentNodes) {
+		this.adjacentNodes = adjacentNodes;
+	}
+
+	public void setContinent(String continent) {
+		this.continent = continent;
+	}
+
+	public String getContinent() {
+		return continent;
+	}	
+}

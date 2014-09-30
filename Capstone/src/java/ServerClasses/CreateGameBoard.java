fc6459698f29a9a08546958cfe5f11383014d362
PatrickGibbons <p_l_gibbons@hotmail.co.nz>
2014-10-01 10:40:26 +1300

    Golden fields wasn't adj to Hills of Norvos. Now fixed

--- Capstone/src/java/ServerClasses/CreateGameBoard.java
+++ Capstone/src/java/ServerClasses/CreateGameBoard.java
@@ -26,7 +26,7 @@ public class CreateGameBoard {
 				{"Ashford","The Reach","Skyreach"},	{"Brightwater Keep","Ashford","The Reach","Dorne"},	{"Ashford","Kingslanding"},
 				{"Skyreach","The Disputed Lands"},{"The Flatlands","Hills of Norvos","Braavosian Coastlands"},{"Dragon Stone","The Flatlands","Braavos"},
 				{"Braavosian Coastlands","The Golden Fields","Hills of Norvos"},{"The Flatlands","Hills of Norvos","Volantis","The Disputed Lands"},
-				{"The Golden Fields","Volantis","Dorne"},{"Braavos","Forest of Lohor","Volantis","The Flatlands"},
+				{"The Golden Fields","Volantis","Dorne"},{"Braavos","Forest of Lohor","Volantis","The Flatlands", "The Golden Fields"},
 				{"Hills of Norvos","Northen Dathraki Sea","Volantis"},{"The Golden Fields","The Disputed Lands","Bhonash","Forest of Lohor","Northen Dathraki Sea"},
 				{"Forest of Lohor","Volantis","Bhonash","Village of Lhazareen","Vaes Dorthrak"},{"Valyria","Volantis","Village of Lhazareen","Northen Dathraki Sea"},
 				{"Bhonash"},{"Eastern Dathraki Sea","Palt of Illen","Village of Lhazareen","Northen Dathraki Sea","Bayasabhad"},
